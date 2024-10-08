19bb65a24f91f4ea060b7febca892f2c000d37db
DmitriiDrobin <dmitrydrobin0626@gmail.com>
2024-10-08 22:20:11 +0300

    Add files via upload

---
+++ peremen.py
@@ -0,0 +1,4 @@
+kol_dz = 12
+kol_hour = 1.5
+name_kurs = "Python"
+print("Курс:", name_kurs+",","всего задач:" + str(kol_dz) + ",", "затрачено часов:", str(kol_hour) + ",", "среднее время выполнения", kol_hour / kol_dz, "часа.")
